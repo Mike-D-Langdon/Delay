3ad21a7cd88ae6940179ba2b005bddcc1225e791
Mike Langdon <michael.dean.langdon@gmail.com>
2024-08-17 18:26:23 -0700

    Add skew factor to give small values more room for adjustment.  Add functions to clean up displaying parameter value strings

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -17,6 +17,22 @@ static void castParameter(juce::AudioProcessorValueTreeState& apvts, const juce:
     jassert(destination);  // parameter does not exist or wrong type
 }
 
+static juce::String stringFromMilliseconds(float value, int) {
+    if (value < 10.0f) {
+        return juce::String(value, 2) + " ms";
+    } else if (value < 100.0f) {
+        return juce::String(value, 1) + " ms";
+    } else if (value < 1000.0f) {
+        return juce::String(int(value)) + " ms";
+    } else {
+        return juce::String(value * 0.001f, 2) + " s";
+    }
+}
+
+static juce::String stringFromDecibels(float value, int) {
+    return juce::String(value, 1) + " dB";
+}
+
 Parameters::Parameters(juce::AudioProcessorValueTreeState& apvts)
 {
     castParameter(apvts, gainParamID, gainParam);
@@ -31,13 +47,15 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
         gainParamID,
         "Output Gain",
         juce::NormalisableRange<float> { -12.0f, 12.0f },
-        0.0f));
+        0.0f,
+        juce::AudioParameterFloatAttributes().withStringFromValueFunction(stringFromDecibels)));
     
     layout.add(std::make_unique<juce::AudioParameterFloat>(
         delayTimeParamID,
         "Delay Time",
-        juce::NormalisableRange<float> { minDelayTime, maxDelayTime },
-        100.0f));
+        juce::NormalisableRange<float> { minDelayTime, maxDelayTime, 0.001f, 0.25f },
+        100.0f,
+        juce::AudioParameterFloatAttributes().withStringFromValueFunction(stringFromMilliseconds)));
     
     return layout;
 }
