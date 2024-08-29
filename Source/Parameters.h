e28e68fc98869a1893d704d86d4447c078353765
Mike Langdon <michael.dean.langdon@gmail.com>
2024-08-28 17:28:50 -0700

    Add exponential (one-pole) smoothing to delay time; add wet/dry mix

--- Source/Parameters.h
+++ Source/Parameters.h
@@ -14,6 +14,7 @@
 
 const juce::ParameterID gainParamID { "gain", 1 };
 const juce::ParameterID delayTimeParamID { "delayTime", 1 };
+const juce::ParameterID mixParamID { "mix", 1 };
 
 class Parameters
 {
@@ -29,6 +30,7 @@ public:
     
     float gain = 0.0f;
     float delayTime = 0.0f;
+    float mix = 1.0f;
     
     static constexpr float minDelayTime = 5.0f;
     static constexpr float maxDelayTime = 5000.0f;
@@ -37,8 +39,11 @@ private:
     juce::AudioParameterFloat* gainParam;
     juce::LinearSmoothedValue<float> gainSmoother;
     
+    juce::AudioParameterFloat* mixParam;
+    juce::LinearSmoothedValue<float> mixSmoother;
+    
     juce::AudioParameterFloat* delayTimeParam;
     
-    float targetDelayTime = 0.0f;
+    float targetDelayTime = 0.0f;  // one-pole smoothing
     float coeff = 0.0f;  // one-pole smoothing
 };
