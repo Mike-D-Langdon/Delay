508e6706e45f7c2db1d5cdee201782770ed79801
Mike Langdon <michael.dean.langdon@gmail.com>
2024-09-05 17:23:40 -0700

    Add rotary knob for Gain and make it functional

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,6 +15,17 @@ DelayAudioProcessorEditor::DelayAudioProcessorEditor (DelayAudioProcessor& p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 16);
+    slider.setBounds(0, 0, 70, 86);
+    addAndMakeVisible(slider);
+    
+    label.setText("Gain", juce::NotificationType::dontSendNotification);
+    label.setJustificationType(juce::Justification::horizontallyCentred);
+    label.setBorderSize(juce::BorderSize<int>{ 0, 0, 2, 0 });
+    label.attachToComponent(&slider, false);
+    addAndMakeVisible(label);
+    
     setSize (500, 330);
 }
 
@@ -33,4 +44,5 @@ void DelayAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    slider.setTopLeftPosition(215, 120);
 }
